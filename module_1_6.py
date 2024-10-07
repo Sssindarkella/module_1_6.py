73e6ee5fa9f788437b9f3c138c1e525789d05b7a
Sindarkella <sssindarkella@gmail.com>
2024-10-07 13:43:12 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict={'Anna': 1996, 'Karmen': 2000, 'Mariam': 1997}
+print(my_dict)
+print(my_dict.get('Anna'))
+print(my_dict.get('Mila', 'Данные отсутствуют'))
+my_dict.update({'Nika': 1987, 'Jastin': 1975})
+print(my_dict)
+print(my_dict.pop('Nika'))
+print(my_dict)
+my_set={1,5,6,5,1,'string',(125,8965)}
+print(my_set)
+print(my_set.add(895),my_set.add('Anna'))
+print(my_set)
+print(my_set.remove(895))
+print(my_set)
